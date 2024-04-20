f5206ea834a11a84b3369062ae963b2b231f92b4
Sam <sam.wheat@outlook.com>
2024-04-20 08:11:24 -0700

    Refactor Constants

---
+++ Vyntix.Fred.Domain/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaderAnalytics.Vyntix.Fred.Domain;
+
+internal class Constants
+{
+    public const string DateFormat = "yyyy-MM-dd";
+    public const string DateTimeFormat = "yyyy-MM-dd hh:mm:ss tt";
+    public const string DecimalFormat = "#,##0.0000";
+}
